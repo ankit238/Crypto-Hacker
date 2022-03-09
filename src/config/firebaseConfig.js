ae4bd962977c3ddfc310847f14fdfe4811d5b38d
Ankit Kumar <79011661+ankit238@users.noreply.github.com>
2022-03-09 11:53:22 +0530

    first commit

---
+++ src/config/firebaseConfig.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyD6B-fCaFz0G4NVJ6z64OxEmDBVCjnQRTg",
+  authDomain: "cryptoapp-5fa24.firebaseapp.com",
+  projectId: "cryptoapp-5fa24",
+  storageBucket: "cryptoapp-5fa24.appspot.com",
+  messagingSenderId: "729433105453",
+  appId: "1:729433105453:web:fb9743b5c4fe51fb63c4a4"
+};
+
+export default firebaseConfig;
